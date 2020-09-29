63e40f7f2641a0c818c4f56ad7cba29f32e8318c
Ramon <ramonomello@gmail.com>
2020-09-29 04:20:12 -0300

    Inicio da criação do boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,14 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="React Avançado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" />
+  </S.Wrapper>
 )
 
 export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Main from 'components/Main'
+
 export default function Home() {
-  return <h1>React Avançado</h1>
+  return <Main />
 }
